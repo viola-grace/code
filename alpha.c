f9b21a970761f040d967cbadb97a333aa36bb6f8
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-06-11 10:20:04 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+    {
+        printf("Alphabet");
+    }
+    else{
+        printf("No");
+    }
+}
