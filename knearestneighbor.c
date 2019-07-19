792aad79dfa19c0c82afb337b41001a382acbf37
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-07-19 18:04:11 +0530

    Create knearestneighbor.c

---
+++ knearestneighbor.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+   int n,ele,i,k,j,temp,p;
+   scanf("%d %d",&n,&ele);
+   int arr[n],sub[n],left[n],t;
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&arr[i]);
+       sub[k++]=abs(arr[i]-ele);
+       left[p++]=arr[i];
+   }
+  
+   for(i=0;i<k;i++)
+   {
+       for(j=i+1;j<k;j++)
+       {
+           if(sub[i]>sub[j])
+           {
+           temp=sub[i];
+           sub[i]=sub[j];
+           sub[j]=temp;
+           
+           t=left[i];
+           left[i]=left[j];
+           left[j]=t;
+           }
+       }
+   }
+
+   printf("%d %d %d",left[1],left[2],left[3]);
+   
+}
