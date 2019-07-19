36c4a6a6329bea66722a40424ff1716d586d71cc
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-07-19 21:37:57 +0530

    Create addsuptok.c

---
+++ addsuptok.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+  int n,k,i,j,flg=0;
+  scanf("%d %d",&n,&k);
+  int arr[n];
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+      for(j=i+1;j<n;j++)
+         if(arr[i]+arr[j]==k)
+         {
+            flg=1;
+            break;
+         }
+  }
+  if(flg==0)
+  {
+      printf("NO");
+  }else
+  {
+      printf("YES");
+  }
+}
