280b57c2f6265baba1efac482c0ff08be988eb9c
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-07-21 18:49:10 +0530

    Create arrayrotleft.c

---
+++ arrayrotleft.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+   int n,k,i,arr[100],safe;
+   scanf("%d %d",&n,&k);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&arr[i]);
+   }
+   while(k--)
+   {
+       safe=arr[0];
+       for(i=0;i<n;i++)
+       {
+        arr[i]=arr[i+1];
+       }
+       arr[n-1]=safe;
+   }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
