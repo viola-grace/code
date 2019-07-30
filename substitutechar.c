13c479a9b44ddac54fc081226bc48391b6559b78
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-07-30 21:03:18 +0530

    Update substitutechar.c

--- substitutechar.c
+++ substitutechar.c
@@ -4,8 +4,15 @@ int main() {
     char str1[100],str2[100];
     scanf("%s %s",str1,str2);
     int len1=strlen(str1);
+    int temp;
     int len2=strlen(str2);
-    int t=len2;
+    if(len2<len1)
+    {
+        temp=len1;
+        len1=len2;
+        len2=temp;
+    }
+    int t=len2,star=0;
     int count=0,i=0,j=0,flg=0;
         l:    if(str1[i]==str2[j])
             {
@@ -14,6 +21,8 @@ int main() {
                 flg=0;
                 if(j==len1)
                 {
+                    flg=1;
+                   star=1;
                     goto l1;
                 }
                 for(i=1;i<len2;i++)
@@ -23,6 +32,10 @@ int main() {
                         flg=1;
                         j++;
                         t--;
+                        if(j==len1)
+                        {
+                            printf("0");return 0;
+                        }
                     }
                     else
                     if(flg==1)
@@ -40,8 +53,16 @@ int main() {
                 j++;
                 goto l;
             }
-  l1:      if(flg==0)
+  l1:      if(flg==1&&star==1)
+            {
+                printf("%d",len2-1);return 0;
+            }
+        if(flg==1)
+        {
+            printf("%d",count);
+        }
+    else if(flg==0)
+    {
         printf("%d",len2);
-        else
-    printf("%d",count);
+    }
 }
